7e4b0635e6682bc053ff7180a2614065292fc418
arya <anantya4995@gmail.com>
2026-09-10 23:29:42 +0700

    chore(debug): enable marionette binding in debug mode for e2e testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:marionette_flutter/marionette_flutter.dart';
 import 'app.dart';
 import 'di/injection.dart';
 import 'presentation/theme/theme_bloc.dart';
 import 'presentation/theme/theme_event.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  if (kDebugMode) {
+    MarionetteBinding.ensureInitialized();
+  } else {
+    WidgetsFlutterBinding.ensureInitialized();
+  }
   configureDependencies();
   getIt<ThemeBloc>().add(const LoadInitialThemeEvent());
   runApp(App());
